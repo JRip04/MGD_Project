319dc861ccb6d7c97a33a10eaa4e515fb5b9bd0f
JRip04 <gamerzoom04@gmail.com>
2024-08-11 19:03:42 +1000

    EOS Join/Host Session

--- Source/MGD_Template/Private/MGGameInstance.cpp
+++ Source/MGD_Template/Private/MGGameInstance.cpp
@@ -4,13 +4,22 @@
 #include "MGGameInstance.h"
 #include "OnlineSubsystem.h"
 #include "OnlineSubsystemUtils.h"
+#include "Online/OnlineSessionNames.h"
+
+	UMGGameInstance::UMGGameInstance()
+	{
+		FoundSessions = MakeShareable(new FOnlineSessionSearch());
+		FoundSessions->MaxSearchResults = 10;
+		FoundSessions->bIsLanQuery = true;
+		FoundSessions->QuerySettings.Set(SEARCH_LOBBIES, true, EOnlineComparisonOp::Equals);
+	}
 
 	void UMGGameInstance::Init()
 	{
 		Super::Init();
 
 		//This is the online subsystem
-		//This Gives us access to all of the online subsystem functions
+		//This Gives us access to all the online subsystem functions
 		const IOnlineSubsystem* ossRef = Online::GetSubsystem(GetWorld());
 
 		//If the ossRef Returns Null
@@ -27,12 +36,26 @@
 		//exit the function
 		if (!identityRef)
 			return;
+
+		//Binding the login complete function to the login complete in the identity interface
+		identityRef->OnLoginCompleteDelegates->AddUObject(this, &UMGGameInstance::EosLoginComplete);
+
+		const IOnlineSessionPtr sessionRef = ossRef->GetSessionInterface();
+
+		if(!sessionRef)
+			return;
+
+		sessionRef->OnCreateSessionCompleteDelegates.AddUObject(this, &UMGGameInstance::SessionCreateComplete);
+
+		sessionRef->OnFindSessionsCompleteDelegates.AddUObject(this, &UMGGameInstance::SessionFindComplete);
+
+		sessionRef->OnJoinSessionCompleteDelegates.AddUObject(this, &UMGGameInstance::SessionJoinComplete);
 	}
 
 void UMGGameInstance::loginEOS()
 {
 	//This is the online subsystem
-	//This Gives us access to all of the online subsystem functions
+	//This Gives us access to all the online subsystem functions
 	const IOnlineSubsystem* ossRef = Online::GetSubsystem(GetWorld());
 
 	//If the ossRef Returns Null
@@ -51,7 +74,7 @@ void UMGGameInstance::loginEOS()
 		return;
 
 	// this sets up the type of account credentials that the login will expect
-	// the account portal is a login portal that opens whe you try to login
+	// the account portal is a login portal that opens whe you try to log in
 	FOnlineAccountCredentials accCreds;
 	accCreds.Id = FString();
 	accCreds.Token = FString();
@@ -60,3 +83,150 @@ void UMGGameInstance::loginEOS()
 	// Run the login portal
 	identityRef->Login(0, accCreds);
 }
+
+	bool UMGGameInstance::IsLogedIn() const
+	{
+		//This is the online subsystem
+		//This Gives us access to all the online subsystem functions
+		const IOnlineSubsystem* ossRef = Online::GetSubsystem(GetWorld());
+
+		//If the ossRef Returns Null
+		// exit the function
+		if (!ossRef)
+			return false;
+
+		// this is the identity interface for EOS
+		// we can get info about the players accounts from this interface
+		// it also allows us to access authentication (Login/Logout)
+		const IOnlineIdentityPtr identityRef = ossRef->GetIdentityInterface();
+
+		// if the identityRef returns null
+		//exit the function
+		if (!identityRef)
+			return false;
+
+		return identityRef->GetLoginStatus(0) == ELoginStatus::LoggedIn;
+	}
+
+	FString UMGGameInstance::GetDisplayName() const
+	{
+		if (!IsLogedIn())
+			return FString(TEXT("DisplayName"));
+
+		// Since is logged in already checks that the interface exists we can skip a step straight to the identity
+		const IOnlineIdentityPtr identityRef = Online::GetSubsystem(GetWorld())->GetIdentityInterface();
+
+		return identityRef->GetPlayerNickname(0);
+	}
+
+	bool UMGGameInstance::IsInSession() const
+	{
+		//Are we logged in
+		//this checks if there is a online subsystem, so we can skip that check
+		if(!IsLogedIn())
+			return false;
+
+		//get the interface that interacts with sessions, so we can get session data
+		const IOnlineSessionPtr SessionRef = Online::GetSubsystem(GetWorld())->GetSessionInterface();
+
+		//make sure there is a session interface
+		if (!SessionRef)
+			return false;
+
+		// get and locally store the session state
+		const EOnlineSessionState::Type state = SessionRef->GetSessionState(MGSESSION_NAME);
+
+		//if there is a session return true
+		return state != EOnlineSessionState::NoSession;
+	}
+
+	void UMGGameInstance::HostGame(bool lan)
+	{
+		if (!IsLogedIn())
+			return;
+
+		const IOnlineSessionPtr sessionRef = Online::GetSubsystem(GetWorld())->GetSessionInterface();
+
+		if (!sessionRef)
+			return;
+
+		FOnlineSessionSettings settings;
+		settings.NumPublicConnections = 4;
+		settings.bIsLANMatch = lan;
+		settings.bIsDedicated = false;
+		settings.bAllowInvites = true;
+		settings.bShouldAdvertise = true;
+		settings.bUsesPresence = true;
+		settings.bUseLobbiesIfAvailable = true;
+		settings.Set(SEARCH_KEYWORDS, MGSESSION_NAME.ToString(), EOnlineDataAdvertisementType::ViaOnlineService);
+		settings.Set(SEARCH_LOBBIES, true, EOnlineDataAdvertisementType::ViaOnlineService);
+
+		sessionRef->CreateSession(0,MGSESSION_NAME, settings);
+	}
+
+	void UMGGameInstance::FindAndJoinSession()
+	{
+		if(!IsLogedIn())
+		return;
+
+		const IOnlineSessionPtr sessionRef = Online::GetSubsystem(GetWorld())->GetSessionInterface();
+
+		if(!sessionRef)
+			return;
+
+		sessionRef->FindSessions(0, FoundSessions.ToSharedRef());
+	}
+
+	void UMGGameInstance::EosLoginComplete(int32 LocalUserNum, bool bWasSuccessful, const FUniqueNetId& UserId,
+	                                       const FString& Error)
+	{
+		OnLoginComplete(bWasSuccessful, Error);
+	}
+
+	void UMGGameInstance::SessionCreateComplete(FName SessionName, bool bWasSuccessful)
+	{
+		EnableListenServer(true);
+		OnSessionCreateComplete(bWasSuccessful);
+	}
+
+	void UMGGameInstance::SessionFindComplete(bool bWasSuccessful)
+	{
+		if(!bWasSuccessful)
+		{
+			UE_LOG(LogTemp, Error, TEXT("FAILED TO FIND SESSION"))
+			return;
+		}
+			
+
+		const IOnlineSubsystem* ossRef = Online::GetSubsystem(GetWorld());
+
+		if(ossRef)
+			return;
+
+		const IOnlineSessionPtr sessionRef = ossRef->GetSessionInterface();
+
+		if (!sessionRef)
+			return;
+
+		if(FoundSessions->SearchResults.IsEmpty())
+		{
+			UE_LOG(LogTemp, Error, TEXT("FAILED TO FIND SESSIONS"))
+			return;
+		}
+
+		UE_LOG(LogTemp, Warning, TEXT("FOUND SESSIONS, ATTEMPTING TO JOIN"))
+		sessionRef->JoinSession(0, MGSESSION_NAME, FoundSessions->SearchResults[0]);
+	}
+
+	void UMGGameInstance::SessionJoinComplete(FName SessionName, EOnJoinSessionCompleteResult::Type Result)
+	{
+		OnSessionJoinComplete(Result == EOnJoinSessionCompleteResult::Success);
+		if(Result != EOnJoinSessionCompleteResult::Success)
+		{
+			UE_LOG(LogTemp, Error, TEXT("FAILED TO JOIN SESSION"))
+			return;
+		}
+
+		ClientTravelToSession(0, SessionName);
+	}
+
--- Source/MGD_Template/Public/MGGameInstance.h
+++ Source/MGD_Template/Public/MGGameInstance.h
@@ -4,7 +4,12 @@
 
 #include "CoreMinimal.h"
 #include "Engine/GameInstance.h"
+#include "OnlineSessionSettings.h"
+#include "Interfaces/OnlineSessionInterface.h"
 #include "MGGameInstance.generated.h"
+
+
+#define MGSESSION_NAME FName(TEXT("MGSESSION"))
 
 /**
  * 
@@ -16,15 +21,50 @@ class MGD_TEMPLATE_API UMGGameInstance : public UGameInstance
 
 	// Allows other Classes to Use that function
 public:
+	UMGGameInstance();
+	
 	virtual void Init() override;
 	
 	UFUNCTION(BlueprintCallable, Category="Login")
 	void loginEOS();
 
-	// Only this class and Classes Derived can use this function
-protected: 
+	UFUNCTION(BlueprintPure, Category="login")
+	bool IsLogedIn() const;
+
+	UFUNCTION(BlueprintPure, Category="User")
+	FString GetDisplayName() const;
+
+	UFUNCTION(BlueprintPure, Category="Session")
+	bool IsInSession() const;
+
+	UFUNCTION(BlueprintCallable, Category="Session")
+	void HostGame(bool lan);
+
+	UFUNCTION(BlueprintCallable, Category="Session")
+	void FindAndJoinSession();
 	
+
+	// Only this class and Classes Derived can use this function
+protected:
+	UFUNCTION(BlueprintImplementableEvent, Category="Login")
+	void OnLoginComplete(bool success, const FString& Error);
+	
+	UFUNCTION(BlueprintImplementableEvent, Category="Session")
+	void OnSessionCreateComplete(bool success);
+
+	UFUNCTION(BlueprintImplementableEvent,Category="Session")
+	void OnSessionJoinComplete(bool success);
+
 	// Only this class can access these functions
 private:
-	
+	//Runs When Eos Login Completes
+	void EosLoginComplete(int32 LocalUserNum, bool bWasSuccessful, const FUniqueNetId& UserId, const FString& Error);
+
+	void SessionCreateComplete(FName SessionName, bool bWasSuccessful);
+
+	void SessionFindComplete(bool bWasSuccessful);
+
+	void SessionJoinComplete(FName SessionName, EOnJoinSessionCompleteResult::Type Result);
+
+	TSharedPtr<FOnlineSessionSearch> FoundSessions;
 };
